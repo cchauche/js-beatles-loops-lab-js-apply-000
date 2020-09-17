5fbeac862eba39282ffffcb5daa3dbbee2a9d110
Colin Chauche <colin.chauche@gmail.com>
2020-09-17 00:36:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments) {
+  let whatTheMembersPlay = [];
+  for (let i = 0; i < musicians.length; i++) {
+    let musicanPlays = musicians[i] + ' plays ' + instruments[i];
+    whatTheMembersPlay.push(musicanPlays);
+  }
+}
